a59b960b1810102f67b3b212cc5a21dd4c8670c8
jettagozoom <johnmarksjr@gmail.com>
2019-04-13 21:07:51 -0600

    Finished Using Services & Dependency Injection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  accounts = [
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
+
+  constructor(private accountsService: AccountsService) {}
+  /*accounts = [
     {
       name: 'Master Account',
       status: 'active'
@@ -19,13 +24,19 @@ export class AppComponent {
       name: 'Hidden Account',
       status: 'unknown'
     }
-  ];
+  ];*/
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
   }
 
+/*  onAccountAdded(newAccount: {name: string, status: string}) {
+    // this.accounts.push(newAccount);
+    this.accountsService.addAccount(name, status);
+  }*/
+
   onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+    // this.accounts[updateInfo.id].status = updateInfo.newStatus;
+    this.accountsService.updateStatus(updateInfo.id, updateInfo.newStatus);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,40 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import {LoggingService} from '../logging.service';
+import {AccountsService} from '../accounts.service';
+
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css']
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  // @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  // LoggingService and AccountsService is dependenc injected when added through
+  // the constructor.
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountsService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
+    this.accountService.addAccount(accountName, accountStatus);
+/*    this.accountAdded.emit({
       name: accountName,
       status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    });*/
+
+    // You could get access to the LoggingService this way, but this is not
+    // dependency injected, which is really bad. Let's do it the Angular way!
+/*    const service = new LoggingService();
+    service.logStatusChange(accountStatus);*/
+    // console.log('A server status changed, new status: ' + accountStatus);
+
+    // Do this in the AccountsService instead
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
